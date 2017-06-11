6773d485177ecd7363352d81cc14a9f2bbd5a6eb
Niqui <niqui@Niquis-MacBook-Air.local>
2017-06-11 06:15:58 -0700

    added loan length, bugs

--- libraryproject/libraryapp/models.py
+++ libraryproject/libraryapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+from django.utils import timezone
 
 MONTHS = (
     (1, "January"),
@@ -26,9 +27,7 @@ class Book(models.Model):
 	small_image_url = models.URLField(max_length=200, null=True, blank=True)
 	publisher = models.CharField(max_length=256, null=True)
 	num_pages = models.IntegerField(null=True)
-	series_title = models.CharField(max_length=256, null=True)
-	series_id = models.IntegerField(null=True, blank=True, default=0)
-	pubyear = models.IntegerField(null=True, blank=True)
+	pubyear = models.IntegerField(null=True, blank=True, default=0)
 	pubday = models.IntegerField(null=True, blank=True)
 	pubmonth = models.IntegerField(choices=MONTHS, null=True, blank=True)
 	rating = models.CharField(max_length=256, null=True)
@@ -37,16 +36,50 @@ class Book(models.Model):
 
 	@property
 	def date(self):
-		return "{} {}, {}".format(self.get_pubmonth_display(), self.pubday, self.pubyear)
-	@property
-	def series(self):
-		if self.series_title == None:
-			return "None"
-		else:
-			return "<a href='http://www.goodreads.com/series/{}' target='_blank'>{}</a>".format(self.series_id, self.series_title)
+		if self.pubmonth != None and self.pubday != None and self.pubyear != None:
+			return "{} {}, {}".format(self.get_pubmonth_display(), self.pubday, self.pubyear)
+		elif self.pubmonth == None and self.pubday == None and self.pubyear == None:
+			return "No Date Avaliable"
+		elif self.pubmonth == None and self.pubday == None:
+			return "{}".format(self.pubyear)
+		elif self.pubmonth != None and self.pubyear != None:
+			return "{} {}".format(self.get_pubmonth_display(), self.pubyear)
+		elif self.pubday != None and self.pubyear != None:
+			return "{}".format(self.pubyear)
+			 
 class Loans(models.Model):
 
 	date_created = models.DateTimeField(auto_now_add=True)
 	borrowed_from = models.CharField(max_length=256)
 	borrower = models.CharField(max_length=256, null=True)
 	book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+	@property
+	def timesince(self):
+		#print(datetime.datetime.now(timezone.utc))
+		if self.date_created != None:
+			diff = timezone.now().replace(microsecond=0) - self.date_created.replace(microsecond=0)
+			days = diff.days
+			hours = diff.seconds // 3600
+			min = diff.seconds % 3600 / 60
+			if int(min) == 0:
+				return "0 minutes"
+			if days != 0:
+				if hours == 1:
+					return "{} days, {} hour".format(days, hours)
+				else:
+					return "{} days, {} hours".format(days, hours)
+			elif days == 0 and hours == 0:
+				if int(min) == 1:
+					return "{} minute".format(int(min))
+				else:
+					return "{} minutes".format(int(min))
+			elif days == 0:
+				if hours == 1 and int(min) == 1:
+					return "{} hour {} minute".format(hours, int(min))
+				elif int(min) == 1:
+					return "{} hours {} minute".format(hours, int(min))
+				elif hours == 1:
+					return "{} hour {} minutes".format(hours, int(min))
+				else:
+					return "{} hours {} minutes".format(hours, int(min))
--- libraryproject/libraryapp/views.py
+++ libraryproject/libraryapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import ListView, DetailView
+
 import xmltodict, json
 from .forms import BookForm
 from .models import Book, Loans
@@ -69,79 +70,89 @@ def choose(request):
     return render(request, 'choose.html', {'choices':books})
 def add(request):
     id = request.GET.get('id')
-    info = requests.get("https://www.goodreads.com/book/show.xml?id=%s&key=lTxFf0cwiHnsUItGsSIX9g"%id)
-    data = xmltodict.parse(info.text)
-    info = {}
-    info['title'] = data['GoodreadsResponse']['book']['title']
-    try:
-        info['author'] = data['GoodreadsResponse']['book']['authors']['author']['name']
-        info['author_id'] = data['GoodreadsResponse']['book']['authors']['author']['id']
-    except:
-        info['author'] = data['GoodreadsResponse']['book']['authors']['author'][0]['name']
-        info['author_id'] = data['GoodreadsResponse']['book']['authors']['author'][0]['id']
-    info['isbn'] = data['GoodreadsResponse']['book']['isbn']
-    info['small_image_url'] = data['GoodreadsResponse']['book']['small_image_url']
-    info['publisher'] = data['GoodreadsResponse']['book']['publisher']
-    info['num_pages'] = data['GoodreadsResponse']['book']['num_pages']
-    info['url'] = data['GoodreadsResponse']['book']['url']
-    try:
-        info['series_title'] = data['GoodreadsResponse']['book']['series_works']['series_work']['series']['title']
-        info['series_id'] = data['GoodreadsResponse']['book']['series_works']['series_work']['series']['id']
-    except:
-        info['series_title'] = data['GoodreadsResponse']['book']['series_works']
-    info['year'] = data['GoodreadsResponse']['book']['publication_year']
-    print(info['year'])
-    info['day'] = data['GoodreadsResponse']['book']['publication_day']
-    info['month'] = data['GoodreadsResponse']['book']['publication_month']
-    info['description'] = data['GoodreadsResponse']['book']['description']
+    if id != None:
+        info = requests.get("https://www.goodreads.com/book/show.xml?id=%s&key=lTxFf0cwiHnsUItGsSIX9g"%id)
+        data = xmltodict.parse(info.text)
+        info = {}
+        info['title'] = data['GoodreadsResponse']['book']['title']
+        try:
+            info['author'] = data['GoodreadsResponse']['book']['authors']['author']['name']
+            info['author_id'] = data['GoodreadsResponse']['book']['authors']['author']['id']
+        except:
+            info['author'] = data['GoodreadsResponse']['book']['authors']['author'][0]['name']
+            info['author_id'] = data['GoodreadsResponse']['book']['authors']['author'][0]['id']
+        info['isbn'] = data['GoodreadsResponse']['book']['isbn']
+        info['small_image_url'] = data['GoodreadsResponse']['book']['small_image_url']
+        info['publisher'] = data['GoodreadsResponse']['book']['publisher']
+        info['num_pages'] = data['GoodreadsResponse']['book']['num_pages']
+        info['url'] = data['GoodreadsResponse']['book']['url']
+        info['year'] = data['GoodreadsResponse']['book']['publication_year']
+        info['day'] = data['GoodreadsResponse']['book']['publication_day']
+        info['month'] = data['GoodreadsResponse']['book']['publication_month']
+        info['description'] = data['GoodreadsResponse']['book']['description']
 
-    return render(request, 'form.html', {'info':info})
+        return render(request, 'form.html', {'info':info})
+    else:
+        return render(request, 'form.html', {})
     
 def lend(request):
     if request.method == 'POST':
         form = BookForm(request.POST)
         if form.is_valid():
+            print(form.cleaned_data)
             book = Book()
-            book.title = request.POST['title']
-            book.author = request.POST['author']
-            book.author_id = request.POST['author_id']
-            book.ibsn = request.POST['isbn']
-            book.small_image_url = request.POST['image_url']
-            book.publisher = request.POST['publisher']
-            book.num_pages = request.POST['num_pages']
-            book.series_title = request.POST['series_title']
-            if request.POST['series_id'] == '':
-                book.series_id = 0
-            else:
-                book.series_id = request.POST['series_id']
-            if request.POST['year'] != 'None':
-            	book.pubyear = request.POST['year']
-            if request.POST['day'] != 'None':
-            	book.pubday = request.POST['day']
-            if request.POST['month'] != 'None':
-            	book.pubmonth = request.POST['month']
-            book.url = request.POST['url']
-            book.description = request.POST['description']
+            if request.POST['title'] != 'None' or request.POST['title'] != '\n':
+                book.title = request.POST['title']
+            if request.POST['author'] != 'None' or request.POST['author'] != '\n':
+                book.author = request.POST['author']
+            if request.POST['author_id'] != 'None' or request.POST['author_id'] != '\n':
+                book.author_id = request.POST['author_id']
+            if request.POST['isbn'] != 'None' or request.POST['isbn'] != '\n':
+                book.ibsn = request.POST['isbn']
+            if request.POST['image_url'] != 'None' or request.POST['image_url'] != '\n':
+                book.small_image_url = request.POST['image_url']
+            if request.POST['publisher'] != 'None' or request.POST['publisher'] != '\n':
+                book.publisher = request.POST['publisher']
+            if request.POST['num_pages'] != 'None' or request.POST['num_pages'] != '\n':
+                book.num_pages = request.POST['num_pages']
+            if request.POST['num_pages'] == 'None':
+            	book.num_pages = 0
+            if request.POST['year'] != 'None' or request.POST['year'] != '\n':
+                book.pubyear = request.POST['year']
+            if request.POST['day'] != 'None' or request.POST['day'] != '\n':
+                book.pubday = request.POST['day']
+            if request.POST['month'] != 'None' or request.POST['month'] != '\n':
+                book.pubmonth = request.POST['month']
+            if request.POST['url'] != 'None' or request.POST['url'] != '\n':
+                book.url = request.POST['url']
+            if request.POST['year'] == 'None':
+                book.pubyear = 0
+            if request.POST['day'] == 'None':
+                book.pubday = 0
+            if request.POST['month'] == 'None':
+                book.pubmonth = 0
+            if request.POST['url'] != 'None' or request.POST['url'] != '\n':
+                book.url = request.POST['url']
+            if request.POST['description'] != 'None' or request.POST['description'] != '\n':
+                book.description = request.POST['description']
             book.save()
             loan = Loans()
             loan.borrower = request.POST['lendee']
             loan.borrowed_from = request.POST['lender']
             loan.book = book
             loan.save()
-            return HttpResponseRedirect('/books')
+            return HttpResponseRedirect('/loans/')
     else:
         form = BookForm()
 
-    return HttpResponse("success")
+    return HttpResponseRedirect("/loans")
 
 class LoansList(ListView):
     model = Loans
-    #form_class = LoanForm
     ordering = ['-date_created']
     
 class BookList(ListView):
     model = Book
-    #paginate_by = 50
     ordering = ['title']
 
 class BookDetail(DetailView):
